0620a01e95cf49c4a1f94771c086fb20249f2a43
leisyarcia <leisyarcia@gmail.com>
2020-12-09 15:28:30 +0000

    all passed

--- exercises/04.1-conditional-rendering/index.js
+++ exercises/04.1-conditional-rendering/index.js
@@ -4,11 +4,25 @@ import PropTypes from "prop-types";
 
 const Alert = props => {
 	//your component here
-
+	let alertClasses = "alert";
+	if (props.color == "red") {
+		alertClasses += " alert-danger";
+	} else if (props.color == "orange") {
+		alertClasses += " alert-warning";
+	}
+	if (props.test == "green") {
+		alertClasses += " text-success";
+	}
+	return (
+		<div className={alertClasses} role="alert">
+			{props.text}
+		</div>
+	);
 };
 Alert.propTypes = {
 	color: PropTypes.string,
-	text: PropTypes.string
+	text: PropTypes.string,
+	test: PropTypes.string
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
